ed6bd55dfa98a756a4e79d46a9d0fffce0ab4a60
GENU05 <93sharma.bharat@gmail.com>
2019-02-20 13:20:27 +0530

    Bug Fixes.

--- ias/lab5/16IT210_IT352_P5.py
+++ ias/lab5/16IT210_IT352_P5.py
@@ -1,4 +1,29 @@
 import math
+def primeFactors(n): 
+    l = []
+    # Print the number of two's that divide n 
+    while n % 2 == 0: 
+        # print (2 ) 
+        l.append(2)
+        n = n / 2
+          
+    # n must be odd at this point 
+    # so a skip of 2 ( i = i + 2) can be used 
+    for i in range(3,int(math.sqrt(n))+1,2): 
+          
+        # while i divides n , print i ad divide n 
+        while n % i== 0: 
+            # print(i)
+            l.append(i)
+            n = n / i 
+              
+    # Condition if n is a prime 
+    # number greater than 2 
+    if n > 2: 
+        # print(n) 
+        l.append(n)
+    return l
+
 #z=x^c mod n
 def sq(x,c,n):
     # print("\nFormat is z=x^c mod n")
@@ -54,11 +79,34 @@ def enc(l,k,e,n):
         return True 
     return False
 
+def gcd(a, b): 
+      
+    # Everything divides 0  
+    if (a == 0 or b == 0): 
+            False
+      
+    # base case 
+    if (a == b): 
+        return a 
+  
+    # a is greater 
+    if (a > b): 
+        return gcd(a-b, b) 
+          
+    return gcd(a, b-a) 
+      
 
 def main():
     k = input('Cipher Text:')
     e = int(input('e: '))
     n = int(input('N: ')) 
+    l = primeFactors(n) 
+    if len(l)!=2:
+        print('Incorrect p & q ')
+    p , q = l[0] , l[1] 
+    phi = (p-1) * (q-1)
+    if gcd(phi,e)!=1:
+        print('E and Phi are not co-prime.')
     algo(k,e,n)
 
 
